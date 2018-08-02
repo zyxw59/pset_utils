2a557352d69f4949397b938ba55d68b8e1132735
Emily Crandall Fleischman <emilycf@mit.edu>
2018-08-02 15:28:04 -0700

    Simplify Figure setup

--- pset_utils/__init__.py
+++ pset_utils/__init__.py
@@ -1,15 +1,18 @@
 """
 A module that provides useful functionality for pyplot.
 
-```Figure = figure_setup(pyplot)```
-in an interactive shell, or
-```Figure = figure_setup(pyplot, False)```
-in a script, will allow you to use
+The `Figure` class will allow you to use
 ```
 with Figure(name, figsize=figsize) as fig:
     # plot things
 ```
 to easily display or save plots.
+
+`Figure.set_interactive(False)` will cause `Figure`s to be saved on close.
+To set the file extension for saved figures, use `Figure.set_extension(suffix)`
+
+Note that if a non-default matplotlib backend is selected, it must be done
+before loading this module.
 
 To use monochromatic lines:
     pyplot.rcParams.update(monochrome_params)
@@ -21,6 +24,8 @@ import contextlib
 from cycler import cycler
 import csv
 import numpy as np
+from matplotlib import pyplot as plt
+import sys
 
 # print monochromatic lines
 monochrome = (cycler('marker', ['', '.']) *
@@ -39,68 +44,69 @@ pgf_params = {
 # default figure size
 FIGSIZE = (6, 6)
 
-class _Figure(contextlib.AbstractContextManager):
+class Figure(contextlib.AbstractContextManager):
     """A context manager to create and display or save a figure."""
 
+    _interactive = True
     _number = 0
+    _suffix = ''
 
     def __init__(self, *args, **kwargs):
         if not args:
             self.name = self._number
-            _Figure._number += 1
+            Figure._number += 1
         elif isinstance(args[0], str):
             self.name = args[0]
             args = args[1:]
         else:
-            self.fig = self._plt.figure()
+            self.fig = plt.figure()
             with self:
-                self._plt.plot(*args, **kwargs)
+                plt.plot(*args, **kwargs)
             return
         if 'figsize' not in kwargs:
             kwargs['figsize'] = FIGSIZE
-        self.fig = self._plt.figure(*args, **kwargs)
+        self.fig = plt.figure(*args, **kwargs)
 
     def __enter__(self):
         return self.fig
 
     def __exit__(self, *args):
         if self._interactive:
-            self._plt.show()
+            plt.show()
         else:
             self.fig.savefig(self.name + self._suffix)
-            self._plt.close(self.fig)
+            plt.close(self.fig)
+
+    @classmethod
+    def set_interactive(cls, interactive):
+        """Sets whether the Figure is used in an interactive context."""
+        cls._interactive = interactive
+
+    @classmethod
+    def set_extension(cls, suffix):
+        """Sets the extension to be used when saving files.
+
+        A value of `None` disables the addition of extensions.
+        """
+        cls._suffix = '.{}'.format(suffix) if suffix is not None else ''
 
     @classmethod
     def fig_3d(cls, *args, **kwargs):
         """Returns a `Figure3d` instance"""
-        return _Figure3d(*args, **kwargs)
+        return Figure3d(*args, **kwargs)
 
 
-class _Figure3d(_Figure):
+class Figure3d(Figure):
     """A context manager to create and display or save a 3d figure."""
 
     def __enter__(self):
         return self.fig.add_subplot(1, 1, 1, projection="3d")
 
+
 def figure_setup(pyplot, interactive=True, suffix=None):
-    """Returns a context manager for displaying or saving figures.
-
-    Args:
-        pyplot: The pyplot module
-        interactive (optional): If true (default), this is being run from an
-            interactive shell, and the context manager will display the figure
-            upon exiting.  Otherwise, the figure will be saved upon exiting.
-        suffix (optional): If provided, all filenames will be suffixed with a
-            period followed by `suffix`.
-
-    Returns:
-        A context manager for displaying or saving figures.
-    """
-    _Figure._plt = pyplot
-    _Figure._interactive = interactive
-    _Figure._suffix = '.{}'.format(suffix) if suffix is not None else ''
-    return _Figure
-
+    print("This function has been removed. "
+          "Simply use the `Figure` class provided in this module.",
+          file=sys.stderr)
 
 def numpy_from_csv(filename, quoting=csv.QUOTE_NONNUMERIC, **kwargs):
     """Load a csv file into a numpy structured array.
